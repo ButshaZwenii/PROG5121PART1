932e09fc4a2542166670519b2a6f516cccc2e75f
ButshaZweni <zwenibutsha@gmail.com>
2026-09-09 01:09:59 +0200

    Update Login.java
    Fixed variable naming inconsistencies in login.Java

--- PROG5121PART1/src/main/java/Login.java
+++ PROG5121PART1/src/main/java/Login.java
@@ -7,7 +7,7 @@ public class Login {
     
     // declaring variable 
     
-    private String Username;
+    private String userName;
     private String password;
     private String cellNumber;
     private String firstName;
@@ -20,8 +20,8 @@ public class Login {
     Runs when you create a new object. It sets up the initial values for your fields.
     */
     
-    public login (String username , String password, String  cellNumber , String firstName, String lastName){
-        this.Username = username;
+    public Login (String username , String password, String  cellNumber , String firstName, String lastName){
+        this.userName = userName;
         this.password = password;
         this.cellNumber = cellNumber;
         this.firstName = firstName;
@@ -34,7 +34,7 @@ public class Login {
     Allow you to read private data from outside the class safely.
     */
     
-    public String getUsername() { return username; }
+    public String getUsername() {return userName;}
     public String getPassword(){return password;}
     public String getCellNumber(){return cellNumber;}
     public String getFirstName(){ return firstName;}
@@ -47,13 +47,10 @@ public class Login {
     
 
     
-    public void setUsername(String username) {this.Username = username;}
+    public void setUsername(String username) {this.userName = username;}
     public void setPassword(String password) {this.password = password;}
     public void setCellNumber(String cellNumber) {this.cellNumber = cellNumber;}
     public void setFirstname(String firstName) {this.firstName = firstName;}
     public void setLastNmae (String lastName) {this.lastName = lastName;}
-    
-    
-    
-    
+   
 }
